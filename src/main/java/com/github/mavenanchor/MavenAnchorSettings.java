53a9a8b4c68d03c134f76eeb9c1b815be4b30a2c
israelpinto <hartl123452019@gmail.com>
2026-02-03 22:17:01 +0200

    only format the files no logic change

--- src/main/java/com/github/mavenanchor/MavenAnchorSettings.java
+++ src/main/java/com/github/mavenanchor/MavenAnchorSettings.java
@@ -1,59 +1,36 @@
 package com.github.mavenanchor;
 
-
-
 import com.intellij.openapi.components.PersistentStateComponent;
-
 import com.intellij.openapi.components.Service;
-
 import com.intellij.openapi.components.State;
-
 import com.intellij.openapi.components.Storage;
-
 import com.intellij.openapi.project.Project;
-
 import com.intellij.util.xmlb.XmlSerializerUtil;
-
 import org.jetbrains.annotations.NotNull;
-
 import org.jetbrains.annotations.Nullable;
 
-
-
 @Service(Service.Level.PROJECT)
-
-@State(name = "MavenAnchorSettings", storages = @Storage("MavenAnchor.xml"))
-
+@State(
+        name = "MavenAnchorSettings",
+        storages = @Storage("MavenAnchor.xml")
+)
 public final class MavenAnchorSettings implements PersistentStateComponent<MavenAnchorSettings> {
 
     public String mavenHome = "";
-
     public String userSettings = "";
 
-
-
     public static MavenAnchorSettings getInstance(Project project) {
-
         return project.getService(MavenAnchorSettings.class);
-
     }
-
-
 
     @Nullable
-
     @Override
-
-    public MavenAnchorSettings getState() { return this; }
-
-
-
-    @Override
-
-    public void loadState(@NotNull MavenAnchorSettings state) {
-
-        XmlSerializerUtil.copyBean(state, this);
-
+    public MavenAnchorSettings getState() {
+        return this;
     }
 
+    @Override
+    public void loadState(@NotNull MavenAnchorSettings state) {
+        XmlSerializerUtil.copyBean(state, this);
+    }
 }
--- src/main/java/com/github/mavenanchor/MavenSetupActivity.java
+++ src/main/java/com/github/mavenanchor/MavenSetupActivity.java
@@ -1,77 +1,37 @@
 package com.github.mavenanchor;
 
-
-
 import com.intellij.openapi.project.Project;
-
 import com.intellij.openapi.startup.ProjectActivity;
-
 import kotlin.Unit;
-
 import kotlin.coroutines.Continuation;
-
 import org.jetbrains.annotations.NotNull;
-
 import org.jetbrains.annotations.Nullable;
-
 import org.jetbrains.idea.maven.project.MavenProjectsManager;
-
 import org.jetbrains.idea.maven.project.MavenGeneralSettings;
-
-
-
-import java.io.File;
-
-
 
 public class MavenSetupActivity implements ProjectActivity {
 
-
-
     @Nullable
-
     @Override
-
     public Object execute(@NotNull Project project, @NotNull Continuation<? super Unit> continuation) {
-
-// Change these strings to your actual local paths
-
+        // Change these strings to your actual local paths
         String myCustomMavenHome = "C:/Program Files/Maven/apache-maven-3.9.6";
-
         String myCustomSettingsXml = "C:/Users/IsraelPinto/.m2/custom-settings.xml";
-
-
 
         MavenProjectsManager manager = MavenProjectsManager.getInstance(project);
 
-
-
         if (manager != null) {
-
             MavenGeneralSettings settings = manager.getGeneralSettings();
 
-
-
-// Set the Maven Home path
-
+            // Set the Maven Home path
             settings.setMavenHome(myCustomMavenHome);
 
-
-
-// Set the User Settings File path
-
+            // Set the User Settings File path
             settings.setUserSettingsFile(myCustomSettingsXml);
 
-
-
             System.out.println("MavenAnchor: Successfully applied paths to " + project.getName());
-
         }
 
-
-
         return Unit.INSTANCE;
-
     }
-
 }
