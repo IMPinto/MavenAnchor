80338cba08788e9030ef3d48fa23be3f802321fb
GitHub Action <action@github.com>
2026-02-03 13:25:27 +0000

    Template cleanup

--- src/main/kotlin/com/github/impinto/mavenanchor/MyBundle.kt
+++ src/main/kotlin/com/github/impinto/mavenanchor/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.impinto.mavenanchor
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/impinto/mavenanchor/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/impinto/mavenanchor/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.impinto.mavenanchor.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.impinto.mavenanchor.MyBundle
+import com.github.impinto.mavenanchor.services.MyProjectService
 import javax.swing.JButton
 
 
